59fbf682e67a6233e4bcf7799ba0a4aa9894cc7a
admodev <admosdigital@yahoo.com>
2020-07-31 11:44:51 -0300

    cambios menores

--- confirmacion.php
+++ confirmacion.php
@@ -5,3 +5,16 @@ include('email_verification.php');
 
 ?>
 
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Generate</title>
+</head>
+<body>
+<div>
+<h3>Por favor confirma tu cuenta para continuar...</h3>
+<p>Si ya activaste tu cuenta <a href="login.php">Logueate!</a></p>
+</div>
+</body>
+</html>
