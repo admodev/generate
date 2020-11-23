cc4f90ec4cb0d4d2b3910a2cae9a0ae56124af36
admodev <admosdigital@yahoo.com>
2020-11-22 21:57:30 -0300

    cambios menores, bug fixes

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['username'])) : ?>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Generate</title>
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/normalize.css" />
   <link rel="stylesheet" href="./css/estilos.min.css" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
@@ -54,7 +54,7 @@ if (isset($_SESSION['username'])) : ?>
             <a class="nav-link text-white" href="visionMision.php">Visión y Misión</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#" id="irBeneficios" data-toggle="modal" data-target="#beneficiosModal">Beneficios</a>
+            <a class="nav-link text-white" href="listaBeneficios.php" id="irBeneficios" data-toggle="modal" data-target="#beneficiosModal">Beneficios</a>
             <div class="modal fade" id="beneficiosModal" tabindex="-1" role="dialog" aria-labelledby="beneficiosModalLabel" aria-hidden="true">
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -80,11 +80,6 @@ if (isset($_SESSION['username'])) : ?>
           </li>
         </ul>
         <a href="register.php" class="nav-link nav-link-registerphp"><i class="fas fa-user-plus">
-            <?php
-            if (isset($_SESSION['username'])) {
-              header('location: perfil.php');
-            }
-            ?>
           </i></a>
       </div>
     </nav> <!-- /NAVBAR -->
@@ -307,8 +302,8 @@ if (isset($_SESSION['username'])) : ?>
   <script src="./src/index.js"></script>
   <!-- BOOTSTRAP JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ listaBeneficios.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title></title>
+</head>
+<body>
+    <div class="container h-100 mt-5">
+  <div class="row h-100 justify-content-center align-items-center">
+    <div class="d-flex flex-column">
+    <a href="beneficios-descuentos.php" class="modal-beneficios m-1">Descuentos</a>
+    <a href="beneficios-prestamos.php" class="modal-beneficios m-1">Prestamos</a>
+    <a href="beneficios-proyectos.php" class="modal-beneficios m-1">Proyectos</a>
+    <a href="beneficios-ayudasocial.php" class="modal-beneficios m-1">Ayuda Social</a>
+    <a href="beneficios-red.php" class="modal-beneficios m-1">Mi Red</a>
+    </div>
+  </div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
